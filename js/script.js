d3bb95c9f375aff953898ce05f4be25d4a73f201
mik3Mac <mik3Mac@michaelMac.local>
2024-01-02 23:33:26 +0700

    update 2 Jan 24

--- js/script.js
+++ js/script.js
@@ -6,14 +6,26 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Klik di luar sidebar untuk menghilangkan Nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
-});
 
-// Toggle class active untuk search form
-const searchForm = document.searchForm(".search-form");
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
