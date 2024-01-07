efea19ad92fe4e4ba62e0d5fbb16c03ed1b854e4
mik3Mac <mik3Mac@michaelMac.local>
2024-01-08 00:14:17 +0700

    update 8 Jan 24

--- src/app.js
+++ src/app.js
@@ -3,19 +3,134 @@ document.addEventListener("alpine:init", () => {
     items: [
       {
         id: 1,
-        name: "Robusta Temanggung",
-        img: "1.1.jpg",
+        name: "Earlybird Coffee",
+        img: "1.jpg",
         price: 20000,
       },
       {
         id: 2,
-        name: "Arabica Sidikalang",
-        img: "2.1.jpg",
+        name: "Boldy Go Coffee",
+        img: "2.jpg",
         price: 35000,
       },
-      { id: 3, name: "Robusta Lampung", img: "3.1.jpg", price: 25000 },
-      { id: 4, name: "Arabica Toraja", img: "4.1.jpg", price: 45000 },
-      { id: 5, name: "Robusta Flores", img: "5.1.jpg", price: 30000 },
+      { id: 3, name: "Bay Coffee", img: "3.jpg", price: 25000 },
+      { id: 4, name: "Little Nap Coffee", img: "4.jpg", price: 45000 },
+      { id: 5, name: "Mazevo Coffee", img: "5.jpg", price: 30000 },
     ],
   }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      // cek apakah ada barang yang sama di cart
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+
+      // jika belum ada / cart masih kosong
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        // jika barang sudah ada, di cek apakah barang sama atau beda dengan yan ada di cart
+        this.items = this.items.map((item) => {
+          // jika barang berbeda
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            // jika barang sudah ada, tambah quantity dan sub totalnya
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+
+    remove(id) {
+      // ambil yang mau di remove berdasarkan id nya
+      const cartItem = this.items.find((item) => item.id === id);
+
+      // jika item lebih dari 1
+      if (cartItem.quantity > 1) {
+        // telusur satu persatu
+        this.items = this.items.map((item) => {
+          // jika bukan barang yang di klik
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        // jika barangnya sisa 1
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
 });
+
+// Form Validasi
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+form.addEventListener("keyup", () => {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+// Kirim data ketika tombol checkout di klik
+checkoutButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  const message = formatMessage(objData);
+  window.open("http://wa.me/6285742418689?text=" + encodeURIComponent(message));
+  // console.log(objData);
+});
+
+// Format pesan Whatsapp
+const formatMessage = (obj) => {
+  return `Data Customer :
+    Nama: ${obj.name}
+    Email: ${obj.email}
+    No.HP: ${obj.phone}  
+
+Data Pesanan :
+    ${JSON.parse(obj.items).map(
+      (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n   `
+    )}
+TOTAL : ${rupiah(obj.total)}
+  
+
+Terima Kasih`;
+};
+
+// Konversi ke rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
